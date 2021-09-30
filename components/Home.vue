7fb7bfd26e38aa4821f3e049b9ac29b1e5e959f8
Adetutu <adetutugbangbola@gmail.com>
2021-09-30 18:39:34 +0100

    update on ui

--- components/Home.vue
+++ components/Home.vue
@@ -1,38 +1,39 @@
 <template>
-<div class="container pt-5">
+<div class="container pt-5"> 
+  <h1 class="header-col" style='cursor:pointer'><span class="U-span">U</span>nsplash</h1>
   
-  <NuxtLink to="/">
-  <h1 class="header-col"><span class="U-span">U</span>nsplash</h1>
-  </NuxtLink>
-  <!-- <Modal-Popup /> -->
-
-
-    <div class="pb-5" style=" margin: 0 auto;">
-    <form class="" style="margin:0 auto"  >
-    <input class="w-75 py-2" style="border:3px solid red; margin:0 auto" type="text" placeholder="search photos" v-model="myInput">
-   <button type="submit" @click.prevent= "selectPhotos"
-                       class="btn  sub-btn" style="border:1px solid red">search </button>
-    </form>
-    </div>
+    <form class='ml-2 pb-3 pt-4' @submit.prevent= "selectPhotos">
+  <div class="input-container">
+    <input class="" type="text" required  placeholder="Search Photos"
+    v-model="myInput" />
+    <!-- <button class="button btn  sub-btn" type="submit" @click.prevent= "selectPhotos">Search</button> -->
+    <div>
+         <button type="submit"  :disabled="mesg_in_submission"
+          class="button btn btn-lg sub-btn">Send</button>
+          </div> 
+  </div>
+</form>
 
  <div class="row">
 
-    <div class="col-md-4"  v-for="data in unsplashImg" :key="data.id">
-<b-button id="show-btn" class="pt-0" style="background:none; border:none" @click="$bvModal.show('bv-modal-example')">
+    <NuxtLink to="" class="col-md-4"  v-for="data in unsplashImg" :key="data.id">
 
-  <img class="img-img w-100 mt-5" style="border:5px solid pink"  :src="data.urls.regular" alt="image">
-       </b-button>
-  </div>
 
+  <img class="img-img w-100 mt-5" syle="border:5px solid pink"  :src="data.urls.regular" alt="image">
+
+  </NuxtLink>
+<!--
 <div class="" v-for="data in unsplashImg" :key="data.id">
   <b-modal id="bv-modal-example" class="" style="border:none" hide-footer>
    
-    <img class="img-img w-50 mt-5" style="border:5px solid pink"  :src="data.urls.regular" alt="image">
+    <img class="img-img w-50 mt-5" stle="border:5px solid pink"  :src="data.urls.regular" alt="image">
    
   </b-modal>
+  </div>-->
   </div>
   </div>
-  </div>
+ 
+
 </template>
 
 <script>
@@ -43,51 +44,42 @@
 
   setup() {
     const unsplashImg = ref('')
-    // const name = ref('')
-    // let mesg_in_submission= ref( false );
     let clientId = ref('')
+    let clientIdd = ref('')
     let myInput = ref('')
     let query = ref('')
+    let mesg_in_submission= ref( false );
 
      
 
-    // useFetch(async () => {
+    useFetch(async () => {
      
-    //   await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
-    //   // await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
-    //  .then( response => unsplashImg.value = response?.data.results)
-    // //  .then( console.log('hello', unsplashImg.value))
-    // //  .then(info=> console.log('hello', info))
-    //   .catch(error => console.log(error))
-    // })
-    useFetch(async (e) => {
-       let clientId=process.env.VUE_APP_API_KEY
-      //  let query = flower.value
-       const url = `https://api.unsplash.com/search/photos?&query=flower&client_id=${clientId}`
-        axios.get(url)
-         .then( response => unsplashImg.value = response?.data.results)
-       
-     
-    //   await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
-    //   // await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
-    //  .then( response => unsplashImg.value = response?.data.results)
-    // //  .then( console.log('hello', unsplashImg.value))
-    // //  .then(info=> console.log('hello', info))
-    //   .catch(error => console.log(error))
+      await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=8ol4_VssgaET9oFTpTaMB85YaM38KmeqCh4gDjIezmY')
+      // await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
+     .then( response => unsplashImg.value = response?.data.results)
+    //  .then( console.log('hello', unsplashImg.value))
+    //  .then(info=> console.log('hello', info))
+      .catch(error => console.log(error))
     })
 
+    // useFetch(async (e) => {
+    //    let clientIdd=process.env.VUE_APP_API_KEY
+    //   //  let query = flower.value
+    //   //  const url = `https://api.unsplash.com/search/photos?&query=flower&client_id=${clientIdd}`
+    //    const url = `https://api.unsplash.com/search/photos?&query=flower&client_id=8ol4_VssgaET9oFTpTaMB85YaM38KmeqCh4gDjIezmY`
+    //     axios.get(url)
+    //      .then( response => unsplashImg.value = response?.data.results)
+       
+     
+    // //   await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
+    // //   // await axios.get('https://api.unsplash.com/search/photos?query=flower&client_id=process.env.VUE_APP_API_KEY')
+    // //  .then( response => unsplashImg.value = response?.data.results)
+    // // //  .then( console.log('hello', unsplashImg.value))
+    // // //  .then(info=> console.log('hello', info))
+    // //   .catch(error => console.log(error))
+    // })
 
 
-  //   const invalidFeedback = computed(() => {
-  //  if (name.value.length > 0) {
-  //        return 'Enter at least 4 characters.'
-  //       }
-  //     return  'Please enter something.'
-    
-  // })
-  //  onMounted (() => {
-  //   console.log(process.env.VUE_APP_API_KEY)
-  //  })
 
        const selectPhotos  = (e) => {
         let clientId=process.env.VUE_APP_API_KEY  
@@ -98,34 +90,25 @@
         const url = `https://api.unsplash.com/search/photos?&query=${query}&client_id=${clientId}`
         axios.get(url)
          .then( response => unsplashImg.value = response?.data.results)
-        console.log('hello', query)
-        console.log(e)
+        // console.log('hello', query)
+        // console.log(e)
     //  .then( response => unsplashImg.value = response?.data.results)
     //  .then( console.log('hello', unsplashImg.value))
     //  .then(info=> console.log('hello', info))
       // .catch(error => console.log(error))
 
-      // mesg_in_submission.value = true;
+      mesg_in_submission.value = true;
+
+      myInput.value=''
        }
 
- 
-  myInput.value=''
-
-
-     
-    return { unsplashImg, selectPhotos, clientId, myInput,query, }
-    // return { unsplashImg, invalidFeedback, name }
+    return { unsplashImg, selectPhotos, clientId, myInput,query, mesg_in_submission, clientIdd }
   }
 }
   
 </script>
 
 <style scoped>
-.form-grp{
-    margin: 0 auto;
-    
-    
-}
 
 .header-col{
 color:rgb(104, 103, 103);
@@ -136,4 +119,67 @@ font-style: italic;
 font-size: 3rem;
 color: rgb(255, 0, 128);
 }
+
+form {        
+        width: 98%; 
+       
+    }
+    .input-container {
+        
+        display: flex;
+        background-color: #FFF;
+        
+        
+    }
+
+    input[type=text], .button {
+        
+        padding: .7rem 1rem;
+    
+    }
+    
+  
+    input[type=text] {
+        
+        flex-grow: 2;
+        
+        outline: none;
+        border: none;
+        
+        font-size: 1rem;
+        
+    }
+    
+    
+    
+    
+    .button {
+        background-color: rgb(285, 0, 128);
+         color:whitesmoke;
+        cursor: pointer;
+        border-radius: 0.3rem 1rem 0.3rem 1rem;
+        -webkit-transition: background-color .25s ease-in-out;
+        -moz-transition: background-color .25s ease-in-out;
+        -o-transition: background-color .25s ease-in-out;
+        transition: background-color .25s ease-in-out;
+        
+    }
+    
+    .button:hover {     
+        
+        border: 2px solid  rgb(285, 0, 128);
+      color: yellowgreen;
+      background: inherit;
+    }
+   
+    .button p { 
+        color: #FFF;  
+    }
+    
+    
+    input[type=text]::placeholder {
+        
+        font-size: 1rem;
+        
+    }
 </style>
